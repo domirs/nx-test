d27fa3af84a9418fc81e1a315d81a1c5c753f356
Dominik Forster <domi@imod.ch>
2022-07-14 16:42:50 +0200

    feat(api): change title

--- libs/api/src/lib/api.tsx
+++ libs/api/src/lib/api.tsx
@@ -6,7 +6,7 @@ export interface ApiProps {}
 export function Api(props: ApiProps) {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to Api!</h1>
+      <h1>Welcome to API!</h1>
     </div>
   );
 }
