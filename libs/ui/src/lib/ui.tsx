23ad556d7447f5af4e4b04408690dbfa996d1625
Dominik Forster <domi@imod.ch>
2022-07-14 15:30:20 +0200

    fix(ui): change the title

--- libs/ui/src/lib/ui.tsx
+++ libs/ui/src/lib/ui.tsx
@@ -6,7 +6,7 @@ export interface UiProps {}
 export function Ui(props: UiProps) {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to Ui!</h1>
+      <h1>Welcome to UI!</h1>
     </div>
   );
 }
